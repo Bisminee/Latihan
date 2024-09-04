24cbaa9d8cfb4a7f1a9f9f5bca427bc0ac3a44e3
Bisminee <fiksilmiah@gmail.com>
2024-09-04 19:22:35 +0700

    Latihan 1

--- test.java
+++ test.java
@@ -3,7 +3,9 @@ public class test {
     public static void main(String[] args) {
         Scanner cin = new Scanner(System.in);
         int umur = cin.nextInt();
-
+        cin.nextLine();
+        String nama = cin.nextLine();
         System.out.println(umur);
+        System.out.println(nama);
     }
 }
